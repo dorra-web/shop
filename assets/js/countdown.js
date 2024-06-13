dc07bf97fe7f3420d040aac9b65ca8d0d6d7c117
Dorra <140128615+dorra-web@users.noreply.github.com>
2024-06-14 06:43:59 +0700

    Create countdown.js

---
+++ assets/js/countdown.js
@@ -0,0 +1,19 @@
+        function handleClick(event) {
+            event.preventDefault(); 
+            var link = event.currentTarget.href;
+            var countdownElement = document.getElementById('countdown');
+            var timeLeft = 15;
+
+            countdownElement.style.display = 'block';
+            countdownElement.innerText = 'Please Wait in ' + timeLeft + ' second...';
+
+            var countdownInterval = setInterval(function() {
+                timeLeft--;
+                countdownElement.innerText = 'Please Wait in ' + timeLeft + ' second...';
+
+                if (timeLeft <= 0) {
+                    clearInterval(countdownInterval);
+                    window.location.href = link; 
+                }
+            }, 1000);
+        }
