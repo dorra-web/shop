17ae988bee5fbc7aa260c004acea43abca683e11
Dorra <140128615+dorra-web@users.noreply.github.com>
2024-07-27 05:59:08 +0700

    Update countdown.js

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,19 +1,21 @@
-        function handleClick(event) {
-            event.preventDefault(); 
-            var link = event.currentTarget.href;
-            var countdownElement = document.getElementById('countdown');
-            var timeLeft = 10;
+function handleClick(event) {
+    event.preventDefault();
+    var link = event.currentTarget.href;
+    var countdownElement = document.getElementById('countdown');
+    var hiddenLink = document.getElementById('hiddenLink');
+    var timeLeft = 10;
 
-            countdownElement.style.display = 'block';
-            countdownElement.innerText = 'Please Wait in ' + timeLeft + ' seconds...';
+    countdownElement.style.display = 'block';
+    countdownElement.innerText = 'Please Wait in ' + timeLeft + ' seconds...';
 
-            var countdownInterval = setInterval(function() {
-                timeLeft--;
-                countdownElement.innerText = 'Please Wait in ' + timeLeft + ' seconds...';
+    var countdownInterval = setInterval(function() {
+        timeLeft--;
+        countdownElement.innerText = 'Please Wait in ' + timeLeft + ' seconds...';
 
-                if (timeLeft <= 0) {
-                    clearInterval(countdownInterval);
-                    window.open(link, '_blank');
-                }
-            }, 1000);
+        if (timeLeft <= 0) {
+            clearInterval(countdownInterval);
+            hiddenLink.href = link;
+            hiddenLink.click();
         }
+    }, 1000);
+}
