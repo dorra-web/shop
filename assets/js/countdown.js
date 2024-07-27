ef39c0a377f65cd69018be1d4124575113b203ec
Dorra <140128615+dorra-web@users.noreply.github.com>
2024-07-27 16:41:46 +0700

    Update countdown.js

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,1 +1,49 @@
+function getParameterByName(name, url) {
+            if (!url) url = window.location.href;
+            name = name.replace(/[\[\]]/g, '\\$&');
+            var regex = new RegExp('[?&]' + name + '=([^&#]*)'),
+                results = regex.exec(url);
+            if (!results) return null;
+            return decodeURIComponent(results[1].replace(/\+/g, ' '));
+        }
 
+        function base64Decode(str) {
+            try {
+                return decodeURIComponent(atob(str).split('').map(function(c) {
+                    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+                }).join(''));
+            } catch (e) {
+                console.error('Base64 decoding failed:', e);
+                return null;
+            }
+        }
+
+        function handleClick(event) {
+            event.preventDefault(); 
+            var link = event.currentTarget.href;
+            var countdownElement = document.getElementById('countdown');
+            var timeLeft = 10;
+
+            countdownElement.style.display = 'block';
+            countdownElement.innerText = 'Please Wait in ' + timeLeft + ' second...';
+
+            var countdownInterval = setInterval(function() {
+                timeLeft--;
+                countdownElement.innerText = 'Please Wait in ' + timeLeft + ' second...';
+
+                if (timeLeft <= 0) {
+                    clearInterval(countdownInterval);
+                    window.location.href = link; 
+                }
+            }, 1000);
+        }
+
+        // Dapatkan parameter 'link' dari URL
+        var encodedLink = getParameterByName('link');
+        var link = base64Decode(encodedLink);
+        if (link) {
+            document.getElementById('delayedLink').href = link;
+            handleClick({ preventDefault: function() {}, currentTarget: { href: link } });
+        } else {
+            document.getElementById('countdown').innerText = 'Link parameter not found or invalid!';
+        }
