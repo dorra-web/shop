2320f40590245bc4a536a516ddc277f82ef77a7f
Dorra <140128615+dorra-web@users.noreply.github.com>
2024-07-27 15:58:55 +0700

    Update countdown.js

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,28 +1,19 @@
-function handleClick(event) {
-    event.preventDefault();
-    var link = event.currentTarget.href;
-    var countdownElement = document.getElementById('countdown');
-    var timeLeft = 10;
+        function handleClick(event) {
+            event.preventDefault(); 
+            var link = event.currentTarget.href;
+            var countdownElement = document.getElementById('countdown');
+            var timeLeft = 10;
 
-    countdownElement.style.display = 'block';
-    countdownElement.innerText = 'Please Wait in ' + timeLeft + ' seconds...';
+            countdownElement.style.display = 'block';
+            countdownElement.innerText = 'Please Wait in ' + timeLeft + ' second...';
 
-    var countdownInterval = setInterval(function() {
-        timeLeft--;
-        countdownElement.innerText = 'Please Wait in ' + timeLeft + ' seconds...';
+            var countdownInterval = setInterval(function() {
+                timeLeft--;
+                countdownElement.innerText = 'Please Wait in ' + timeLeft + ' second...';
 
-        if (timeLeft <= 0) {
-            clearInterval(countdownInterval);
-
-            // Buka jendela sementara yang akan ditutup
-            var newWindow = window.open('', '_blank');
-            if (newWindow) {
-                newWindow.location = link;
-                newWindow.focus();
-            } else {
-                // Jika jendela tidak dapat dibuka, fallback ke tab yang sama
-                window.location.href = link;
-            }
+                if (timeLeft <= 0) {
+                    clearInterval(countdownInterval);
+                    window.location.href = link; 
+                }
+            }, 1000);
         }
-    }, 1000);
-}
